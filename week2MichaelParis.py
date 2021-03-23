79ac00ca0553b3ebe6569267bee88e737ea5f43a
razrbak501 <70500291+razrbak501@users.noreply.github.com>
2021-03-23 17:14:31 -0500

    Add files via upload

---
+++ week2MichaelParis.py
@@ -0,0 +1,34 @@
+# DSC530
+# Week 2
+# Assignment 2.1, Python Basics
+# Author Michael Paris
+# 03/23/21
+
+# Display the text “Hello World! I wonder why that is always the default coding text to start with”
+# Add two numbers together
+# Subtract a number from another number
+# Multiply two numbers
+# Divide between two numbers
+# Concatenate two strings together (any words)
+# Create a list of 4 items (can be strings, numbers, both)
+# Append an item to your list (again, can be a string, number)
+# Create a tuple with 4 items (can be strings, numbers, both)
+
+
+print("Hello World")
+num1 = 15
+num2 = 2500
+addedNumber = num1 + num2
+print(addedNumber)
+multipliedNumber = num1 * num2
+print(multipliedNumber)
+dividedNumber = num2 / num1
+print(dividedNumber)
+combinedStrings = "Hello" + " " + "World"
+print(combinedStrings)
+createdList = ["Jamaica", "Cancun", "Orlando", "Gulf Shores"]
+print(createdList)
+createdList = createdList + ["Breckenridge"]
+print(createdList)
+createdTuple = ("Ft. Eustis", "Ft. Knox", "Ft. Leavenworth", "Ft. Clayton")
+print(createdTuple)
